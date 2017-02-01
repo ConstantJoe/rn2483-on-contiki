93a5dbdf43605edf59eed99f1a3bd35da57908f6
ConstantJoe <joseph.finnegan@tutanota.com>
2017-02-01 16:52:22 +0000

    Basic cleanup

--- SerialRelay.c
+++ SerialRelay.c
@@ -1,32 +1,7 @@
 /*
- * SerialRelay.c
- *
- * Created: 20/06/2016 11:10:49
- *  Author: sbrown
- */ 
+  Written by J. Finnegan.
+*/
 
-//
-// ATMega128RFA1 program to relay data between uart0 and uart1
-//
-// - LEDs
-// - Serial Line (UART)
-// - Timers
-//
-// AVR Project Configuration Options
-//  General
-//   - device=atmega128rfa1
-//   - 
-//   - optimization=-O0
-// Include Directories
-//   - "./"
-//
-// CPU setup (connect to AVR PRogrammer/Fuses)
-//   Speed=8MHz
-//    - SUT_CKSEL is set to "Internal RC OScillator"
-//    - CKDIV8 not selected
-//
-
-//#include <avr/io.h>
 #include <stdio.h>
 #include <string.h>
 #include "contiki.h"
@@ -40,17 +15,6 @@
 #include "dev/serial-line.h"
 #include "dev/button-sensor.h"
 //#include "lib/sensors.h"
-
-
-int  append(char*s, size_t size, char c) {
-     if(strlen(s) + 1 >= size) {
-          return 0;
-     }
-     int len = strlen(s);
-     s[len] = c;
-     s[len+1] = '\0';
-     return 1;
-}
 
 /*---------------------------------------------------------------------------*/
 PROCESS(example_process, "Main process");
@@ -89,14 +53,12 @@ PROCESS_THREAD(example_process, ev, data)
           buf[strlen(data)]   = '\r';
           buf[strlen(data)+1]   = '\n';
           buf[strlen(data)+2]   = '\0';
-          rs232_print(RS232_PORT_0, buf);
           rs232_print(RS232_PORT_1, buf);
           
     	}
       else if(ev == serial_line_event_message_1) 
       {
           // data coming from RN2483, send it on to PC.
-          rs232_print(RS232_PORT_0, "received line from uart1:\r\n");
           rs232_print(RS232_PORT_0, (char *)data);
           rs232_print(RS232_PORT_0, "\r\n");
       }
@@ -112,8 +74,6 @@ PROCESS_THREAD(button_process, ev, data)
 
 	SENSORS_ACTIVATE(button_sensor);
 
-	//AUTOSTART_PROCESSES(&sensors_process);
-
 	watchdog_stop();
 
 	leds_init();
@@ -121,14 +81,11 @@ PROCESS_THREAD(button_process, ev, data)
   leds_on(LEDS_GREEN);
   leds_on(LEDS_YELLOW);
   leds_on(LEDS_RED);
-  
-	//volatile int on = 1;
 	
 	while(1) {
 
-		  //rs232_print(0, "waiting for button press\n\r");
     	PROCESS_WAIT_EVENT_UNTIL((ev==sensors_event) && (data == &button_sensor));
-    	//rs232_print(0, "flipping leds\n\r");
+
     	if(leds_get())
     	{
 			      leds_off(LEDS_GREEN);
