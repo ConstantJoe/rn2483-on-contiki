12e8f262df79454238f5ab1e617fa248ccc1926d
ConstantJoe <joseph.finnegan@tutanota.com>
2017-02-02 18:26:40 +0000

    Swapping from IPV6 to RIME stack. Adding in example that uses a RIME broadcast.

--- SerialRelay.c
+++ SerialRelay.c
@@ -2,26 +2,33 @@
   Written by J. Finnegan.
 */
 
-//#include <stdio.h>
-//#include <string.h>
-//#include "contiki.h"
 #include "watchdog.h"
-//#include "libs/timer.h"
-
 #include "dev/leds.h"
 #include "dev/rs232.h"
-//#include "dev/uart1.h"
 #include "serial-line-dual.h"
 #include "dev/button-sensor.h"
-//#include "sys/etimer.h"
-#include "radio/rf230bb/rf230bb.c"
+//#include "radio/rf230bb/rf230bb.c"
+#include "net/rime/rime.h"
+//#include "net/rime/broadcast.h"
 
 /*---------------------------------------------------------------------------*/
 PROCESS(relay_process, "Relay process");
 PROCESS(button_process, "Button process");
 PROCESS(timer_process, "Timer process");
 
-AUTOSTART_PROCESSES(&relay_process, &button_process, &timer_process);
+AUTOSTART_PROCESSES(&relay_process, &button_process);
+
+/*from example-broadcast.c*/
+static void
+broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
+{
+  rs232_print(RS232_PORT_0, "Message received.\r\n");
+  rs232_print(RS232_PORT_0, (char *)packetbuf_dataptr());
+  //printf("broadcast message received from %d.%d: '%s'\n",
+  //       from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
+}
+static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
+static struct broadcast_conn broadcast;
 
 /*---------------------------------------------------------------------------*/
 /*process which shows example of use of serial relay*/
@@ -71,8 +78,12 @@ PROCESS_THREAD(relay_process, ev, data)
 /*process which should example of use of button*/
 PROCESS_THREAD(button_process, ev, data)
 {
+  PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+
 	PROCESS_BEGIN();
 	
+  broadcast_open(&broadcast, 129, &broadcast_call);
+
   process_start(&sensors_process, NULL);
 
 	SENSORS_ACTIVATE(button_sensor);
@@ -81,28 +92,35 @@ PROCESS_THREAD(button_process, ev, data)
 
 	leds_init();
 
-  leds_on(LEDS_GREEN);
-  leds_on(LEDS_YELLOW);
-  leds_on(LEDS_RED);
-	
 	while(1) {
 
     	PROCESS_WAIT_EVENT_UNTIL((ev==sensors_event) && (data == &button_sensor));
 
-    	if(leds_get() == 0b00011100)
+    	if(leds_get())
     	{
 			      leds_off(LEDS_GREEN);
-            leds_off(LEDS_YELLOW);
-            leds_off(LEDS_RED);
+            //leds_off(LEDS_RED);
     	}
     	else
     	{
-    		    leds_on(LEDS_GREEN);
-            leds_on(LEDS_YELLOW);
-            leds_on(LEDS_RED);
+            packetbuf_copyfrom("Hello", 6);
+            broadcast_send(&broadcast);
+            rs232_print(RS232_PORT_0, "Message sent\r\n");
+            
+            /*int succ = rf230_send("test",4);
+
+            if(succ)
+            {*/
+            leds_on(LEDS_GREEN);
+            /*
+            else
+            {
+                leds_on(LEDS_RED);
+            }*/
     	}
-    }
-    PROCESS_END();
+  }
+  PROCESS_END();
+
 }
 
 /*---------------------------------------------------------------------------*/
@@ -119,7 +137,7 @@ PROCESS_THREAD(timer_process, ev, data)
   /* Delay 10 seconds */
   etimer_set(&et, CLOCK_SECOND*10);
 
-  rf230_init();
+  //rf230_init();
 
   while(1) {
 
@@ -127,7 +145,7 @@ PROCESS_THREAD(timer_process, ev, data)
      
       rs232_print(RS232_PORT_0, "Timer expired!\r\n");
 
-      int succ = rf230_send("test",4);
+      /*int succ = rf230_send("test",4);
 
       if(succ)
       {
@@ -136,7 +154,7 @@ PROCESS_THREAD(timer_process, ev, data)
       else
       {
         rs232_print(RS232_PORT_0, "Transmit failed!\r\n");
-      }
+      }*/
       
       etimer_reset(&et);
 
